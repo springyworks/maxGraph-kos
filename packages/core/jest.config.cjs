728776b3e69d2370f235629a4ef68032af2fd100
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-12-29 07:24:34 +0100

    ci: upload test coverage results (#298)
    Also rename the artifact storing all examples for clarity.

--- packages/core/jest.config.cjs
+++ packages/core/jest.config.cjs
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  collectCoverageFrom: ['src/**/*.{ts,js}'],
+  coverageReporters: ['lcov', 'text-summary'],
   // preset: 'ts-jest',
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom', // need to access to the browser objects
