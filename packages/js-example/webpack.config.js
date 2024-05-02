f5c43dee59678ec6ade66ff92ba40c15ec7b4b38
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-05-02 09:26:37 +0200

    ci: include js-example in the "build" workflow (#402)
    This ensures that errors are detected sooner.
    
    Also bump webpack dependencies to use the latest webpack version.
    Fix the configuration to make the js-example build pass
      - fix package-lock.json: the globby dependency wasn't found
      - update `webpack.config.js` to make webpack happy (this is a workaround). maxGraph is now
      declared as "type=module" but the import in the js files are not including extension.
    ```
    ERROR in ../core/dist/index.js 192:0-24
    Module not found: Error: Can't resolve './types' in '/dev/maxGraph/packages/core/dist'
    Did you mean 'types.js'?
    BREAKING CHANGE: The request './types' failed to resolve only because it was resolved as fully specified
    (probably because the origin is strict EcmaScript Module, e. g. a module with javascript mimetype, a '*.mjs' file, or a '*.js' file where the package.json contains '"type": "module"').
    The extension in the request is mandatory for it to be fully specified.
    Add the extension to the request.
    ```

--- packages/js-example/webpack.config.js
+++ packages/js-example/webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      // @maxgraph/core is a dependency of this project but don't declare imports with js extension
+      // use this workaround to make webpack happy: https://github.com/webpack/webpack/issues/11467#issuecomment-691873586
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
     ],
   },
   plugins: [
