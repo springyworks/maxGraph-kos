135fb1ffd4285c645a430c2dc80663588a115084
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-08-07 14:36:04 +0200

    chore(js-example): store CSS out of the JS bundle (#497)
    This reduces the size of the JS bundle.
    
    In addition:
      - add hash to the JS bundle name (to improve caching)
      - clean output folder (to ease maintenance)

--- packages/js-example/webpack.config.js
+++ packages/js-example/webpack.config.js
@@ -2,12 +2,18 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// hack to get the webpack mode
+const isDevMode = !process.argv.includes('--mode=production');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devServer: {
     static: './dist',
@@ -16,7 +22,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
       },
       // @maxgraph/core is a dependency of this project but don't declare imports with js extension
       // use this workaround to make webpack happy: https://github.com/webpack/webpack/issues/11467#issuecomment-691873586
@@ -35,5 +41,5 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-  ],
+  ].concat(isDevMode ? [] : [new MiniCssExtractPlugin()]),
 };
